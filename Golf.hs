29c6d85ed4c2e95ca97098ebd8848b1cc3b3094a
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2016-08-01 22:16:47 -0700

    HW 3 ex 2

--- Golf.hs
+++ Golf.hs
@@ -14,3 +14,14 @@ every n xs = map snd $ filter fst $ map (\(a,b) -> (a `mod` n == 0, b)) $ indexe
 
 skips :: [a] -> [[a]]
 skips xs = map (\n -> every (toInteger n) xs) [1..(length xs)]
+
+----------------------------------------
+-- Exercise 2
+----------------------------------------
+
+triplets ::  [a] -> [[a]]
+triplets ns = map (\n -> take 3 (drop n ns)) [0..(length ns - 3)]
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima ns = map (!! 1) $ filter (\[a,b,c] -> (a < b) && (b > c)) (triplets ns)
+
